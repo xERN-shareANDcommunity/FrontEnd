43420bc307cb95bda538214c4c8ff1272ff03e9a
kimyoungyin <yin199859@gmail.com>
2024-04-04 15:41:37 +0900

    issue #192: make DateAndTime shareable in ScheduleProposalModal

--- src/components/Common/ScheduleProposalModal/ScheduleProposalModal.jsx
+++ src/components/Common/ScheduleProposalModal/ScheduleProposalModal.jsx
@@ -1,13 +1,19 @@
 import React, { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 import _ from "lodash";
 import moment from "moment";
 
+import { getScheduleProposals } from "@/features/schedule/schedule-service";
+import { getTimeString } from "@/utils/calendarUtils";
+
 import {
 	ProposalParamsWrapperDiv,
-	RadiosDiv,
+	RecommendedProposalsDiv,
 } from "./ScheduleProposalModal.styles";
 import FormModal from "../Modal/FormModal/FormModal";
+import DateAndTime from "../ScheduleModal/DateAndTime";
 import {
 	DetailTextarea,
 	ScheduleModalLayoutDiv,
@@ -19,21 +25,78 @@ const initialFormValues = {
 	title: "",
 	content: "",
 
-	isAllDay: false,
-	freq: "NONE",
-	interval: "",
-	byweekday: [],
-	until: "",
+	selectedRecommendationIndexes: [],
 };
 
 const ScheduleProposalModal = () => {
-	const [formValues, setFormValues] = useState(initialFormValues);
-	const [isMultiple, setIsMultiple] = useState(false);
 	const prevFormValue = useRef(initialFormValues);
-	const proposalParams = {
-		startDate: moment().format("YYYY-MM-DD"),
-		endDate: null,
-		minDuration: null,
+	const dispatch = useDispatch();
+	const recommendedScheduleProposals = useSelector(
+		({ schedule }) => schedule.recommendedScheduleProposals,
+	);
+	const [formValues, setFormValues] = useState(initialFormValues);
+	const [proposalParams, setProposalParams] = useState({
+		startDateStr: moment().format("YYYY-MM-DD"),
+		startTimeStr: moment().format("HH:mm"),
+		endDateStr: moment().format("YYYY-MM-DD"),
+		endTimeStr: moment().format("HH:mm"),
+		minDuration: 60, // 분
+	});
+
+	const handleDateValueChange = (date, id) => {
+		const value = moment(date).format("YYYY-MM-DD");
+
+		if (id === "strtDate") {
+			setProposalParams((prev) => ({ ...prev, startDateStr: value }));
+		} else if (id === "endDate") {
+			setProposalParams((prev) => ({
+				...prev,
+				startDateStr: prev.startDateStr > value ? value : prev.startDateStr,
+				endDateStr: value,
+			}));
+		}
+	};
+
+	const handleTimeValueChange = (value, id) => {
+		if (id === "startTime") {
+			setProposalParams((prev) => ({
+				...prev,
+				startTimeStr: value,
+				endTimeStr:
+					prev.startDateStr === prev.endDateStr && value >= prev.endTimeStr
+						? value
+						: prev.endTime,
+			}));
+		} else if (id === "endTime") {
+			setProposalParams((prev) => ({
+				...prev,
+				endTimeStr: value,
+			}));
+		}
+	};
+
+	const handleGettingProposal = () => {
+		const checkTimeIsValid = () => {
+			if (proposalParams.startDateStr < proposalParams.endDateStr) {
+				return true;
+			}
+
+			if (proposalParams.startDateStr === proposalParams.endDateStr) {
+				if (proposalParams.startTimeStr < proposalParams.endTimeStr) {
+					return true;
+				}
+
+				toast.error("시작 시간은 종료 시간보다 빨라야 합니다.");
+				return false;
+			}
+
+			toast.error("종료 날짜는 시작 날짜보다 동일하거나 빠를 수 없습니다.");
+			return false;
+		};
+
+		if (checkTimeIsValid()) {
+			dispatch(getScheduleProposals(proposalParams));
+		}
 	};
 
 	const checkIsEmpty = () => {
@@ -43,6 +106,20 @@ const ScheduleProposalModal = () => {
 			content: formValues.content.trim(),
 		};
 		return _.isEqual(trimmedFormValues, prevFormValue.current);
+	};
+
+	const handleSelectRecommendation = (index) => {
+		setFormValues((prev) => {
+			const isAlreadySelected =
+				prev.selectedRecommendationIndexes.indexOf(index) !== -1;
+
+			return {
+				...prev,
+				selectedRecommendationIndexes: isAlreadySelected
+					? prev.selectedRecommendationIndexes.filter((el) => el !== index)
+					: [...prev.selectedRecommendationIndexes, index],
+			};
+		});
 	};
 
 	return (
@@ -59,51 +136,52 @@ const ScheduleProposalModal = () => {
 						setFormValues((prev) => ({ ...prev, content: e.target.value }))
 					}
 				/>
-				<RadiosDiv>
-					<label htmlFor="single">
-						<input
-							type="radio"
-							id="single"
-							checked={!isMultiple}
-							onChange={() => setIsMultiple(false)}
-						/>
-						단일 날짜
-					</label>
-					<label htmlFor="multiple">
-						<input
-							type="radio"
-							id="multiple"
-							checked={isMultiple}
-							onChange={() => setIsMultiple(true)}
-						/>
-						여러 날짜
-					</label>
-				</RadiosDiv>
 				<ProposalParamsWrapperDiv>
-					<div className="content">
-						<h3>일정 추천</h3>
-						<button type="button">
-							{!proposalParams.endDate
-								? "기간"
-								: `${proposalParams.startDate}~${proposalParams.endDate}`}
-						</button>
-						<button type="button">
-							{proposalParams.minDuration
-								? `${proposalParams.minDuration}`
-								: "시간"}
-						</button>
-					</div>
+					<DateAndTime
+						isProposal={true}
+						startDate={proposalParams.startDateStr}
+						startTime={proposalParams.startTimeStr}
+						endDate={proposalParams.endDateStr}
+						endTime={proposalParams.endTimeStr}
+						onDateChange={handleDateValueChange}
+						onTimeChange={handleTimeValueChange}
+					/>
 					<button
 						type="button"
 						disabled={
-							!proposalParams.startDate ||
-							!proposalParams.endDate ||
+							!proposalParams.startDateStr ||
+							!proposalParams.startTimeStr ||
+							!proposalParams.endDateStr ||
+							!proposalParams.endTimeStr ||
 							!proposalParams.minDuration
 						}
+						onClick={handleGettingProposal}
 					>
 						추천받기
 					</button>
 				</ProposalParamsWrapperDiv>
+				<RecommendedProposalsDiv>
+					{recommendedScheduleProposals.map((proposals, index) => (
+						<div key={proposals.startDateTime}>
+							<div>
+								<button
+									type="button"
+									onClick={() => handleSelectRecommendation(index)}
+								>
+									{formValues.selectedRecommendationIndexes.indexOf(index) !==
+										-1 && <div />}
+								</button>
+								<span>
+									{getTimeString(
+										proposals.startDateTime,
+										proposals.endDateTime,
+									)}
+								</span>
+							</div>
+							<button type="button">반복</button>
+						</div>
+					))}
+				</RecommendedProposalsDiv>
 				<SubmitButton onClick={() => {}} disabled={true}>
 					저장하기
 				</SubmitButton>
